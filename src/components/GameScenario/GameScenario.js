e5c2661599de697be2c5f29b73556d924d79b752
Santiago Betancur Graciano <santybg_94@hotmail.com>
2022-05-18 23:00:50 -0500

    Lives and score logic fixed

--- src/components/GameScenario/GameScenario.js
+++ src/components/GameScenario/GameScenario.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Swal from 'sweetalert2';
 import './GameScenario.css'
 import rock from '../../assets/images/rock.png';
 import paper from '../../assets/images/paper.png';
@@ -9,10 +10,13 @@ import wrongAudio from '../../assets/audio/wrong.wav';
 
 
 export default function GameScenario() {
-  let [option, setOption] = useState('');
-  let [opponentOption, setOpponentOption] = useState('');
-  let [playerScore, setPlayerScore] = useState(0);
-  let [playerLives, setPlayerLives] = useState(['1', '2', '3']);
+  let [round, setRound] = useState({
+    option : '',
+    opponentOption: '',
+    playerScore : 0,
+    playerLives: ['1', '2', '3']
+  });
+  let [streak, setStreak] = useState(0);
 
   let frontCards = document.getElementsByClassName('front-little');
   let backCards = document.getElementsByClassName('back-little');
@@ -22,64 +26,86 @@ export default function GameScenario() {
   let correct = new Audio(correctAudio);
   let wrong = new Audio(wrongAudio);
 
-  const setOpponentsCard = () =>{
-    let choices = ['rock', 'paper', 'scissors'];
-    let index = Math.floor(Math.random()*3);
-
-    setOpponentOption(choices[index]);
+  const checkRound = (option) =>{
+    let opponentOption = setOpponentsCard();    
+    const [playerScore, playerLives] = checkConditions(option, opponentOption);     
+    moveCards(option, opponentOption, playerScore, playerLives);
   }
 
-  if(option === 'rock'){
+  const setOpponentsCard = () =>{
+    let choices = ['rock', 'paper', 'scissors'];
+    //let index = Math.floor(Math.random()*3);
+    return choices[0];
+  }
+
+  if(round.option === 'rock'){
     imgSrc = rock
-  }else if (option === 'paper'){
+  }else if (round.option === 'paper'){
     imgSrc = paper
-  }else if(option === 'scissors' ){
+  }else if(round.option === 'scissors' ){
     imgSrc = scissors
   }
 
-  if(opponentOption === 'rock'){
+  if(round.opponentOption === 'rock'){
     opImgSrc = rock
-  }else if (opponentOption === 'paper'){
+  }else if (round.opponentOption === 'paper'){
     opImgSrc = paper
-  }else if(opponentOption === 'scissors' ){
+  }else if(round.opponentOption === 'scissors' ){
     opImgSrc = scissors
   }
 
-  const checkConditions = () => {  
-    console.log('CHEKEALOOO', option, opponentOption, playerScore)  
+  const checkConditions = (option, opponentOption) => {  
     if((option === 'rock' && opponentOption === 'scissors') || (option === 'paper' && opponentOption === 'rock') || (option === 'scissors' && opponentOption === 'paper')){
-      setPlayerScore(playerScore++);
       correct.play();
+      setStreak(++streak);
+      console.log(streak, typeof(streak))
+      let newStats = [round.playerScore+=1, round.playerLives];
+      return newStats;
+
     }else if ((option === 'rock' && opponentOption === 'paper') || (option === 'paper' && opponentOption === 'scissors') || (option === 'scissors' && opponentOption === 'rock')){
       wrong.play();
-      let newLives = playerLives.pop()
-
-      setPlayerLives(newLives);
-      console.log(playerLives);
-      
+      setStreak(0); //TODO: FIX THIS BUG
+      console.log(streak);
+      round.playerLives.pop();
+      let newStats = [round.playerScore, round.playerLives];
+      return newStats;      
+    }else{
+      return [round.playerScore, round.playerLives];
     }
   }    
 
-  const moveCards = () => {
-    setOpponentsCard();
-
+  const moveCards = (option, opponentOption, playerScore, playerLives) => {  
     Array.from(frontCards).map(x=>x.classList.add('move-front'));
-    Array.from(backCards).map(x=>x.classList.add('move-back'));
+    Array.from(backCards).map(x=>x.classList.add('move-back'));    
 
-    checkConditions();
+    if(streak!==0 && streak % 3 === 0){
+      round.playerLives.push((round.playerLives.length+1).toString());
+      setRound({option, opponentOption, playerScore, playerLives});      
+      console.log(playerLives)      
+    }else{
+      setRound({option, opponentOption, playerScore, playerLives});
+    }
 
+    let optionButtons = document.getElementsByClassName('option');
+    Array.from(optionButtons).map(x=> x.setAttribute('disabled', ''));
     setTimeout(() => {
       Array.from(frontCards).map(x=>x.classList.remove('move-front'));
-      Array.from(backCards).map(x=>x.classList.remove('move-back'));
+      Array.from(backCards).map(x=>x.classList.remove('move-back'));      
+      if(round.playerLives.length === 0){
+        Swal.fire(
+          'Game Over',
+          `Your score is: ${round.playerScore}`,
+          'warning'
+        )
+      }else{
+        Array.from(optionButtons).map(x=> x.removeAttribute('disabled'));
+      }
     }, 1500);
   }
-
-  
-
   
   return (
     <section className='game-section'>
-        <div className="score" style={{right: '0', display:'flex', flexFlow:'row'}}><h2>Score: {0}</h2><span style={{display:'flex', flexFlow: 'row'}}><h2 style={{marginLeft: '15rem'}}>Lives:</h2>{playerLives.map(el => <h2 style={{margin: '1.2rem 0.2rem'}} key={el}>&#128151;</h2>)}</span></div>
+        <div className="score" style={{right: '0', display:'flex', flexFlow:'row'}}><h2>Score: {round.playerScore}</h2><span style={{display:'flex', flexFlow: 'row'}}><h2 style={{marginLeft: '25rem'}}>Lives:</h2>{round.playerLives.map(el => <h2 style={{margin: '1.2rem 0.2rem'}} key={el}>&#128151;</h2>)}</span></div>
       <div className="stage">
       <div className="game-card">
       <div className="front-little">
@@ -87,8 +113,8 @@ export default function GameScenario() {
         <h1>Card</h1>
       </div>
       <div className="back-little">
-        <img id='option-image' src={imgSrc} width='200px' alt={option}/>
-        <h1 id='option-name'>{option}</h1>
+        <img id='option-image' src={imgSrc} width='200px' alt={round.option}/>
+        <h1 id='option-name'>{round.option}</h1>
       </div>
     </div>
     <div className="game-card">
@@ -97,15 +123,15 @@ export default function GameScenario() {
         <h1>Card</h1>
       </div>
       <div className="back-little">
-        <img id='option-image' src={opImgSrc} width='200px' alt={option}/>
-        <h1 id='option-name'>{opponentOption}</h1>
+        <img id='option-image' src={opImgSrc} width='200px' alt={round.option}/>
+        <h1 id='option-name'>{round.opponentOption}</h1>
       </div>
     </div>     
       </div>
       <div className="action-buttons">
-        <button className="option rock" onClick={()=>{setOption('rock'); moveCards();}}>Rock</button>
-        <button className="option paper" onClick={()=>{setOption('paper'); moveCards();}}>Paper</button>
-        <button className="option scissors" onClick={()=>{setOption('scissors'); moveCards();}}>Scissors</button>
+        <button className="option rock" onClick={()=>{checkRound('rock');}}>Rock</button>
+        <button className="option paper" onClick={()=>{checkRound('paper');}}>Paper</button>
+        <button className="option scissors" onClick={()=>{checkRound('scissors');}}>Scissors</button>
     </div>        
     </section>
   )
